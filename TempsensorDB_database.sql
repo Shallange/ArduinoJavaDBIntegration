b2b002f1d82b541e833d2b5e51e78f9d7f83ba10
Jimmy <53408265+Shallange@users.noreply.github.com>
2023-10-05 14:28:48 +0200

    Updated Database Schema with Reverse-Engineerering
    Introduced the SQL script for the database schema, which was reverse-engineered from MySQL Workbench visualization.

--- TempsensorDB_database.sql
+++ TempsensorDB_database.sql
@@ -1,32 +1,42 @@
-CREATE DATABASE TempsensorDB_database;
+-- Note: This database schema was initially generated using Hibernate ORM.
+-- It was then visualized in MySQL Workbench, and this SQL script was reverse-engineered from that visualization.
+
+CREATE DATABASE TempsensorDB_database; -- Creating the database. The name "TempsensorDB_database" can be modified based on preference.
 
 USE TempsensorDB_database;
 
-CREATE TABLE TempTable (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    temperatures DECIMAL(5, 2),
-    timestamp DATETIME NOT NULL
-);
+CREATE TABLE `temp_category_table` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `lower_bound` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `upper_bound` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE TempCategory (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    lower_bound DECIMAL(5, 2),
-    upper_bound DECIMAL(5, 2)
-);
+CREATE TABLE `level_table` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `color` varchar(255) DEFAULT NULL,
+  `category_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK146k750ed2lfwvea8xb4tkjee` (`category_id`),
+  CONSTRAINT `FK146k750ed2lfwvea8xb4tkjee` FOREIGN KEY (`category_id`) REFERENCES `temp_category_table` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE Level (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    color VARCHAR(50) NOT NULL,
-    category_id INT,
-    FOREIGN KEY (category_id) REFERENCES TempCategory(id)
-);
+CREATE TABLE `temp_table` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `temperatures` float DEFAULT NULL,
+  `timestamp` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE Warning (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    temp_id INT,
-    level_id INT,
-    timestamp DATETIME NOT NULL,
-    FOREIGN KEY (temp_id) REFERENCES TempTable(id),
-    FOREIGN KEY (level_id) REFERENCES Level(id)
-);
+CREATE TABLE `warning_table` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `timestamp_warning` datetime(6) DEFAULT NULL,
+  `level_id` bigint DEFAULT NULL,
+  `temp_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKh93mqeqi4i6ywskicn3ev7r6b` (`level_id`),
+  KEY `FKnrkdnio4e2umiv1ciyt5ihwpb` (`temp_id`),
+  CONSTRAINT `FKh93mqeqi4i6ywskicn3ev7r6b` FOREIGN KEY (`level_id`) REFERENCES `level_table` (`id`),
+  CONSTRAINT `FKnrkdnio4e2umiv1ciyt5ihwpb` FOREIGN KEY (`temp_id`) REFERENCES `temp_table` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
